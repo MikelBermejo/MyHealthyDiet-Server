9d22a0fad7498e467068ff998d4735d6a2b3b5eb
JBar <101391346+STParadox@users.noreply.github.com>
2022-12-09 10:23:22 +0100

    Update Client.java
    "@Table(name="client",schema="myhealthydiet")" updated to "@Table(name="client",schema="myhealthydietdb")" in line 23

--- src/java/entities/Client.java
+++ src/java/entities/Client.java
@@ -20,7 +20,7 @@ import javax.validation.constraints.NotNull;
  * @author Sendoa
  */
 @Entity
-@Table(name="client",schema="myhealthydiet")
+@Table(name="client",schema="myhealthydietdb")
 public class Client extends User implements Serializable {
 
     private static final long serialVersionUID = 1L;
