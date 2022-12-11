00999f801e47e917ad716987a3d04ac1873b706a
MikelBermejo <mikelbermejofdez@gmail.com>
2022-12-11 16:18:01 +0100

    ingredient

--- src/java/entities/Ingredient.java
+++ src/java/entities/Ingredient.java
@@ -8,25 +8,59 @@ package entities;
 import java.io.Serializable;
 import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 /**
  *
- * @author JulenB
+ * @author Mikel
  */
 @Entity
+@Table(name = "ingredient", schema = "myhealthydietdb")
 public class Ingredient implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    
+    /**
+     * Id of the entity Ingredient
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
         private Integer ingredient_id;
+    
+    /**
+     * Name of the ingredient
+     */
+    @NotNull
     private String ingredientName;
+    
+    /**
+     * Enumeration for the type of food
+     */
+    @NotNull
+    @Enumerated(EnumType.ORDINAL)
     private FoodTypeEnum foodType;
-
-
+    
+    /**
+     * List to join plate with ingredient
+     */
+    @ManyToMany
+    @JoinTable(schema="myhealthydietdb", name = "PlateIngredient")
+    private List<Plate> plates;
+    
+    /**
+     * Boolean that shows if it is in season or if is not
+     */
+    @NotNull
+    private Boolean isInSeason;
+    
     public Ingredient(Integer ingredient_id, String ingredientName, FoodTypeEnum foodType,
                       List<Plate> plates, Boolean isInSeason) {
         this.ingredient_id = ingredient_id;
@@ -38,13 +72,7 @@ public class Ingredient implements Serializable {
     
     public Ingredient() {
     }
-
-    /**
-     * @associates <{client.Plate}>
-     */
-    private List<Plate> plates;
-    private Boolean isInSeason;
-
+    
     public void setIngredient_id(Integer ingredient_id) {
         this.ingredient_id = ingredient_id;
     }
