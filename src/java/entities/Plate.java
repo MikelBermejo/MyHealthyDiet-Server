b69fef65671b58886f663b88c046808697593323
JBar <101391346+STParadox@users.noreply.github.com>
2023-01-11 13:44:19 +0100

    Update Plate.java
    Remove unnecessary import javax.persistence.Lob;

--- src/java/entities/Plate.java
+++ src/java/entities/Plate.java
@@ -11,7 +11,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinTable;
-import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
